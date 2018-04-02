afdf3245087513e98e14637b958ec62652e36423
nlauchande <nlauchande@gmail.com>
2018-04-02 11:24:51 +0200

    Better implementation with an external function

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,18 @@
 use std::fs;
+use std::path::Path;
 use std::fs::File;
 use std::io::Read;
+use std::io::Result;
+
+// A simple implementation of `% cat path`
+fn  get_file(path: &Path) -> std::io::Result<String> {
+    let mut f = File::open(path)?;
+    let mut s = String::new();
+    match f.read_to_string(&mut s) {
+        Ok(_) => Ok(s),
+        Err(e) => Err(e),
+    }
+}
 
 fn main() {
     println!("Hello, world!");
@@ -18,15 +30,11 @@ fn main() {
     println!("With text:\n{}", contents);
 
     for path in paths {
-
-         let mut new_contents = String::new();
-         let mut f = File::open(&path.unwrap().path()).expect("file not found");
-
-         f.read_to_string(&mut new_contents)
-             .expect("something went wrong reading the file");
-
-        contents.push_str(&new_contents)
+        let new_content = get_file(&path.unwrap().path());
+        contents.push_str(&new_content.unwrap())
 
     }
+
+
     println!("{}",contents);
 }
