abb4d2dfb396dcb3425b8c3bc2d7eb2fa3632c6f
nlauchande <nlauchande@gmail.com>
2018-04-03 09:36:26 +0200

    Add dependency on clap

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,10 @@ use std::fs::File;
 use std::io::Read;
 use std::io::Write;
 
+extern crate clap;
+
+use clap::{Arg, App};
+
 fn  get_file(path: &Path) -> std::io::Result<String> {
     let mut f = File::open(path)?;
     let mut s = String::new();
@@ -30,7 +34,21 @@ fn concat_dir(dir_name: &str) -> std::io::Result<String> {
     Ok(contents)
 }
 
+// Todo: Receive input dir and output file as parameters from the command line
 fn main() {
+
+    let matches = App::new("Concatenate")
+        .version("0.0.1")
+        .author("Natu Lauchande <nlauchande@gmail.com>")
+        .about("Concatenate tool for the command line in rust")
+        .arg(Arg::with_name("DIR")
+            .required(true)
+            .takes_value(true)
+            .index(1)
+            .help("directory"))
+        .get_matches();
+
+    let dir = matches.value_of("DIR").unwrap();
 
     let dir = "./resources";
 
